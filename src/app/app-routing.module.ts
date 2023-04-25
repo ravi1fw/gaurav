a5cc728aa8e0369868671d0e28c707b70c845a1c
ravi123 <ravipandey5957@gmail.com>
2023-04-25 21:27:58 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CareerComponent } from './career/career.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'about', component:AboutComponent},
+  {path:"career", component:CareerComponent},
+  {path:"contact-us",component:ContactUsComponent},
+  {path:"sign-up",component:SignUpComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { CareerComponent } from './career/career.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    SignUpComponent,
+    ContactUsComponent,
+    CareerComponent
   ],
   imports: [
     BrowserModule,
